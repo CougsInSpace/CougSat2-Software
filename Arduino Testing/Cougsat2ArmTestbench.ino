688518235f4c1351d350a6b8725e7667cb57590b
Dlko <51176397+Swateam6@users.noreply.github.com>
2026-03-08 19:59:48 +0000

    Joystick testing

--- Arduino Testing/Cougsat2ArmTestbench.ino
+++ Arduino Testing/Cougsat2ArmTestbench.ino
@@ -3,31 +3,47 @@
 
 #define SHOULDER_SERVO_PIN 10
 #define ELBOW_SERVO_PIN 11
-
+#define ROTATION_SERVO_PIN 12
+#define JOYSTICK_X_PIN A2
 elapsedMillis timer;
 
 Servo shoulderServo;
 Servo elbowServo;
+Servo rotationServo;
+int xvalue;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
+  
   //zero
   shoulderServo.attach(SHOULDER_SERVO_PIN);
   shoulderServo.writeMicroseconds(1500);
   elbowServo.attach(ELBOW_SERVO_PIN);
   elbowServo.writeMicroseconds(1500);
+  rotationServo.attach(ROTATION_SERVO_PIN);
+  rotationServo.writeMicroseconds(1500);
 
-
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   //1000 = 360 degrees 1500 == 180 degrees 2000 = 0 degrees
-  float shoulderAngle = 1500; //180 degrees
-  float elbowAngle = 1500;
-  
-  shoulderServo.writeMicroseconds(shoulderAngle);
-  elbowServo.writeMicroseconds(elbowAngle);
+  xvalue = analogRead(JOYSTICK_X_PIN);
+  xvalue = map(xvalue, 0, 1023, 0, 180);
+  Serial.println(xvalue);
+  if(timer<=1000){
+    shoulderServo.write(xvalue);
+    elbowServo.write(180);
+    rotationServo.write(180);
+  }
+  else if (timer <= 2000){
+    shoulderServo.write(xvalue);
+    elbowServo.write(0);
+    rotationServo.write(0);
+  }
+  else{
+    timer = 0;
+  }
+  //elbowServo.writeMicroseconds(elbowAngle);
 }
